1845f4f87ecbac13f2c84ba7e8ddc881ce259855
Groinkb <you@example.com>
2023-08-23 14:06:01 +0200

    add picture for the future rocket loader pre page

--- index.js
+++ index.js
@@ -19,14 +19,3 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 // Preload rocket
-const stars = document.querySelectorAll("#star");
-
-stars.forEach((star) => {
-	let duration = Math.random() * (1.2 - 0.6) + 0.6;
-	star.style.animation = `stars ${duration}s infinite linear`;
-});
-
-window.addEventListener("load", () => {
-	const preloader = document.querySelector(".preloader");
-	preloader.classList.add("preload-finish");
-});
